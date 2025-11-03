bb1ed51e684672e77161c8af700846986063b2ec
rupesh9999 <91906525+rupesh9999@users.noreply.github.com>
2025-11-03 09:12:07 +0530

    Refactor GKE Terraform configuration
    Updated GKE cluster configuration to use zonal setting and added min_master_version variable. Adjusted firewall rules and cleaned up formatting.

--- gke-terraform/main.tf
+++ gke-terraform/main.tf
@@ -1,80 +1,72 @@
 resource "google_compute_network" "custom_network" {
-  name                    = "custom-vpc"
+  name = "custom-vpc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "custom-subnet" {
-  name          = "custom-subnetwork"
+  name = "custom-subnetwork"
   ip_cidr_range = "10.10.0.0/16"
-  region        = var.region
-  network       = google_compute_network.custom_network.id
+  region = var.region
+  network = google_compute_network.custom_network.id
 }
 
 # Firewall-1 for Internal Communication
 resource "google_compute_firewall" "allow-internal" {
-  name    = "internal-firewall"
+  name = "internal-firewall"
   network = google_compute_network.custom_network.id
-
   allow {
     protocol = "all"
   }
-
   source_ranges = ["10.10.0.0/16"]
 }
 
 # Firewall-2 for External Access SSH, icmp, RDP
 resource "google_compute_firewall" "allow-external" {
-  name    = "external-firewall"
+  name = "external-firewall"
   network = google_compute_network.custom_network.id
-
   allow {
     protocol = "icmp"
   }
-
   allow {
     protocol = "tcp"
-    ports    = ["22", "3389"]
+    ports = ["22", "3389"]
   }
-
   source_ranges = ["0.0.0.0/0"]
 }
 
 # Firewall-3 for GKE Communication
 resource "google_compute_firewall" "allow-gke" {
-  name    = "gke-firewall"
+  name = "gke-firewall"
   network = google_compute_network.custom_network.id
-
   allow {
     protocol = "tcp"
-    ports    = ["443", "10250", "15017"]
+    ports = ["443", "10250", "15017"]
   }
-
   source_ranges = ["0.0.0.0/0"]
 }
 
-
-# GKE Cluster
+# GKE Cluster (Changed to zonal to reduce SSD quota needs)
 resource "google_container_cluster" "primary" {
-  project             = var.project
-  name                = "terraform-gke-cluster"
-  location            = var.region
-  network             = google_compute_network.custom_network.id
-  subnetwork          = google_compute_subnetwork.custom-subnet.id
+  project = var.project
+  name = "terraform-gke-cluster"
+  location = var.zone  # Changed from var.region to var.zone for zonal cluster
+  min_master_version = var.K8s_version  # Use the defined variable to pin version
+  network = google_compute_network.custom_network.id
+  subnetwork = google_compute_subnetwork.custom-subnet.id
   deletion_protection = false
   remove_default_node_pool = true
-  initial_node_count       = 1  
+  initial_node_count = 1
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name           = "my-node-pool"
-  cluster        = google_container_cluster.primary.name
-  location       = google_container_cluster.primary.location
-  node_count     = 1  
-
+  name = "my-node-pool"
+  cluster = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location  # Will now be zonal
+  node_count = 1
   node_config {
-    machine_type   = "e2-medium"  # Use a smaller instance to reduce storage requirements
-    disk_size_gb   = "15"
-    disk_type      = "pd-standard"
-    image_type     = "UBUNTU_CONTAINERD"
+    machine_type = "e2-medium" # Use a smaller instance to reduce storage requirements
+    disk_size_gb = "15"
+    disk_type = "pd-standard"
+    image_type = "UBUNTU_CONTAINERD"
   }
 }
